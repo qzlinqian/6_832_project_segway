40ce9d1dc8b4359626bb24f230573851c4e71570
Qian Lin <qzlinqian@126.com>
2022-04-24 17:07:06 -0400

    init commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# This is a sample Python script.
+import pydrake.all
+
+if __name__ == '__main__':
+    print(pydrake.__file__)
---
+++ obstacle.py
@@ -0,0 +1,3 @@
+# Created by qian at 4/24/22
+
+# Description: class to define physical data for obstacles
