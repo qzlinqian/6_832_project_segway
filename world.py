f0b966d864636bb8623cc691650872d949dc9cce
Qian Lin <qzlinqian@126.com>
2022-04-28 13:19:03 -0400

    add route and vis

--- world.py
+++ world.py
@@ -3,6 +3,7 @@
 # Description: some basic class
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 # class to store segway physical data
@@ -41,15 +42,38 @@ class StaticRoundObstacle(object):
 
 class World(object):
 
-    def __init__(self, num_obs):
+    def __init__(self, num_obs=5):
         self.segway = Segway(2, 4, 0.4, 0.1, 0.6, 1.0, 5.0)
+        self.ref_line_points = []
         self.obstacles = []
 
+        for i in range(100):
+            self.ref_line_points.append([i / 25, 0])
+        for i in range(100):
+            self.ref_line_points.append([4 + np.sin(i * np.pi * 0.01) * 0.5, 0.5 - np.cos(i * np.pi * 0.01) * 0.5])
+        for i in range(101):
+            self.ref_line_points.append([4 - i / 25, 1])
         for i in range(num_obs):
-            x = 1 + 2 * i
-            y = np.random.uniform(-1, 1)
-            radius = np.random.randint(0, 10) * 0.1
+            x = 1 + i
+            y = np.random.uniform(-1, 2)
+            radius = np.random.randint(5, 10) * 0.03
             if radius < 0.01:
                 continue
             self.obstacles.append(StaticRoundObstacle([x, y], radius))
 
+    def visualize(self):
+        figure, axes = plt.subplots()
+        positions = np.asarray(self.ref_line_points)
+        plt.plot(positions[:, 0], positions[:, 1])
+        for obs in self.obstacles:
+            circle = plt.Circle(obs.position, radius=obs.radius)
+            axes.set_aspect(1)
+            axes.add_artist(circle)
+        plt.ylim([-2, 2])
+        plt.xlim([-0.1, 10])
+        plt.show()
+
+
+if __name__ == '__main__':
+    world = World(4)
+    world.visualize()
